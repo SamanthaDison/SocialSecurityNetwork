146c62cd2841312276860095caf49b18e2114bbc
SamanthaDison <samnicdis@gmail.com>
2022-01-14 15:22:27 -0700

    create post button styled; needs modal

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,12 @@
 import { AuthController } from './Controllers/AuthController.js'
 import { ValuesController } from './Controllers/ValuesController.js'
+import { PostsController } from './Controllers/PostsController.js';
+import { Post } from './Models/Post.js';
 
 class App {
   authController = new AuthController();
   valuesController = new ValuesController();
+  postsController = new PostsController();
 }
 
 // @ts-ignore
