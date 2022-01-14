7841a33331671a5e43fd4c9d4ef4131ecc3270e1
Tony Ware <ton8675309@gmail.com>
2022-01-14 10:20:19 -0700

    1st push

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-mn0i566y.us.auth0.com'
+export const audience = 'https://Ssn.com'
+export const clientId = 'pL1nBYaPRs2gkHyTW8TUYXzhRyMOiMrG'
