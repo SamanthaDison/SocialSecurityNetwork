d3656537c92ba84363be193eadbd89fea1919b24
Tony Ware <ton8675309@gmail.com>
2022-01-14 19:19:07 -0700

    working on removes

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -1,8 +1,10 @@
 import { ProxyState } from "../AppState.js";
 import { postsService } from "../Services/PostsService.js";
+import { getPostForm } from "../Component/PostForm.js"
 
 
-function _drawAllPosts() {
+function _drawPosts() {
+    // const posts = ProxyState.posts
     let template = ''
     ProxyState.posts.forEach(p => template += p.Template)
     document.getElementById('posts').innerHTML = template
@@ -10,6 +12,57 @@ function _drawAllPosts() {
 
 export class PostsController {
     constructor() {
-        ProxyState.on('posts', _drawAllPosts)
+        ProxyState.on('posts', _drawPosts)
+        postsService.getAllPosts()
+        console.log('posts')
     }
+    drawPosts() {
+        _drawPosts()
+        document.getElementById('modal-body-slot').innerHTML = getPostForm()
+    }
+
+    async createPost(id) {
+        try {
+            window.event.preventDefault()
+            console.log('submitted')
+            const form = window.event.target
+            const postData = {
+                title: form.title.value,
+                body: form.body.value
+
+            }
+            if (id == "undefined") {
+                await postsService.createPost(postData)
+            }
+            else {
+                await postsService.editPost(postData, id)
+            }
+            form.reset()
+
+            bootstrap.Modal.getOrCreateInstance(document.getElementById('new-listing')).hide()
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+
+    async editPost(id) {
+        try {
+            let foundPost = Proxystate.posts.find(p => p.id == id)
+            bootstrap.Modal.getOrCreateInstance(document.getElementById('new-listing')).toggle()
+            document.getElementById('modal-body-slot').innerHTML = getPostForm(foundPost)
+
+        } catch (error) {
+            consolog('error message in posts controller')
+        }
+    }
+
+    async removePost(id) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
 }
